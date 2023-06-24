ddaddb5a862548cf9e136fe09b5d0f2052c2897a
aneesaro <speaktoanees@gmail.com>
2023-06-24 11:03:11 +0530

    dev commit

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
+
 setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Its a mlops prject',
+    description='Its a mlops project',
     author='anees_aro',
     license='MIT',
 )
